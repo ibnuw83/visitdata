88910deca6f4a23e95be9a0d873ab45bf2d4b4a4
Ibnu Wibowo <ibnuw83@gmail.com>
2025-10-17 03:25:09 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,9 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Logo } from '@/components/logo';
 import Link from 'next/link';
-import { useFormState, useFormStatus } from 'react-dom';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
+import { useFormStatus } from 'react-dom';
+import { Alert, AlertDescription } from '@/components/ui/alert';
+import { useEffect, useActionState } from 'react';
 
 function SubmitButton() {
   const { pending } = useFormStatus();
@@ -22,7 +21,7 @@ function SubmitButton() {
 }
 
 export default function LoginPage() {
-  const [state, formAction] = useFormState(login, undefined);
+  const [state, formAction] = useActionState(login, undefined);
 
   useEffect(() => {
     if (state?.success) {
