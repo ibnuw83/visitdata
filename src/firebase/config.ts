f1d230f293246a83c1216b81bd00e41f2f57d989
Ibnu Wibowo <ibnuw83@gmail.com>
2025-10-18 15:33:57 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAHKE8Oe7i5Uqb-kL8elzrjHdrTCdOM

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,7 +7,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAHKE8Oe7i5Uqb-kL8elzrjHdrTCdOMK_0",
   authDomain: "wisatavisits.firebaseapp.com",
   projectId: "wisatavisits",
-  storageBucket: "wisatavisits.appspot.com",
+  storageBucket: "wisatavisits.firebasestorage.app",
   messagingSenderId: "594192326469",
   appId: "1:594192326469:web:5e3aee38f781d21ae4682c",
   measurementId: "G-JLXZF9PWYT"
