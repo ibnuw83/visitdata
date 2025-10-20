48f065a69dc5f673894aef24b1571036ba42a707
Ibnu Wibowo <ibnuw83@gmail.com>
2025-10-20 05:50:45 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,6 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    // Path to Tremor's node_modules package
-    './node_modules/@tremor/react/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     transparent: 'transparent',
@@ -58,90 +56,11 @@ export default {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
-        // Tremor colors
-        tremor: {
-          brand: {
-            faint: 'hsl(var(--primary) / 0.1)',
-            muted: 'hsl(var(--primary) / 0.3)',
-            subtle: 'hsl(var(--primary) / 0.5)',
-            DEFAULT: 'hsl(var(--primary))',
-            emphasis: 'hsl(var(--primary))',
-            inverted: 'hsl(var(--primary-foreground))',
-          },
-          background: {
-            muted: 'hsl(var(--muted))',
-            subtle: 'hsl(var(--secondary))',
-            DEFAULT: 'hsl(var(--background))',
-            emphasis: 'hsl(var(--card))',
-          },
-          border: {
-            DEFAULT: 'hsl(var(--border))',
-          },
-          ring: {
-            DEFAULT: 'hsl(var(--ring))',
-          },
-          content: {
-            subtle: 'hsl(var(--muted-foreground))',
-            DEFAULT: 'hsl(var(--secondary-foreground))',
-            emphasis: 'hsl(var(--foreground))',
-            strong: 'hsl(var(--foreground))',
-            inverted: 'hsl(var(--background))',
-          },
-        },
-         // dark mode
-        'dark-tremor': {
-          brand: {
-            faint: 'hsl(var(--primary) / 0.1)',
-            muted: 'hsl(var(--primary) / 0.3)',
-            subtle: 'hsl(var(--primary) / 0.5)',
-            DEFAULT: 'hsl(var(--primary))',
-            emphasis: 'hsl(var(--primary))',
-            inverted: 'hsl(var(--primary-foreground))',
-          },
-          background: {
-            muted: 'hsl(var(--muted))',
-            subtle: 'hsl(var(--secondary))',
-            DEFAULT: 'hsl(var(--background))',
-            emphasis: 'hsl(var(--card))',
-          },
-          border: {
-            DEFAULT: 'hsl(var(--border))',
-          },
-          ring: {
-            DEFAULT: 'hsl(var(--ring))',
-          },
-          content: {
-            subtle: 'hsl(var(--muted-foreground))',
-            DEFAULT: 'hsl(var(--secondary-foreground))',
-            emphasis: 'hsl(var(--foreground))',
-            strong: 'hsl(var(--foreground))',
-            inverted: 'hsl(var(--background))',
-          },
-        },
-      },
-      boxShadow: {
-        // light
-        'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-        'tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-        'tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
-        // dark
-        'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-        'dark-tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-        'dark-tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
-         'tremor-small': '0.375rem',
-        'tremor-default': '0.5rem',
-        'tremor-full': '9999px',
-      },
-      fontSize: {
-        'tremor-label': ['0.75rem', { lineHeight: '1rem' }],
-        'tremor-default': ['0.875rem', { lineHeight: '1.25rem' }],
-        'tremor-title': ['1.125rem', { lineHeight: '1.75rem' }],
-        'tremor-metric': ['1.875rem', { lineHeight: '2.25rem' }],
       },
       keyframes: {
         'accordion-down': {
@@ -167,34 +86,5 @@ export default {
       },
     },
   },
-  safelist: [
-    {
-      pattern:
-        /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected'],
-    },
-    {
-      pattern:
-        /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected'],
-    },
-    {
-      pattern:
-        /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-      variants: ['hover', 'ui-selected'],
-    },
-    {
-      pattern:
-        /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-    },
-    {
-      pattern:
-        /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-    },
-    {
-      pattern:
-        /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-    },
-  ],
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/forms'), require('@tremor/react/dist/plugin')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/forms')],
 } satisfies Config;
