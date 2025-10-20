ac63951772653922243348cd988df8188ca9c9cf
Ibnu Wibowo <ibnuw83@gmail.com>
2025-10-20 05:54:59 +0000

    masih eror

---
+++ vercel/project/next.config.ts
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverExternalPackages: ['bcrypt'],
+};
+
+export default nextConfig;
